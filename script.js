a3bae45b3dde3c6da6e5dde9ecdcac60161ce09f
andrew gormly <agormlydev@gmail.com>
2021-09-15 07:51:50 +0000

    add styling, interest calc

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    const principalElement = document.getElementById('principal');
+    const principal = parseFloat(principalElement.value);
+    const rate = document.getElementById('rate').value;
+    const years = document.getElementById('years').value;
+    const interest = (principal * years * rate) / 100;
+    const year = new Date().getFullYear() + parseInt(years);
+    const resultDiv = document.getElementById('result');
+
+    if (principal < 1 || isNaN(principal)) {
+        alert('Enter a positive number');
+        principalElement.focus();
+        return;
+    }
+
+    resultDiv.innerHTML = `If you deposit <mark>${principal}</mark>, <br/>
+                            at an interest rate of <mark>${rate}%</mark>. <br/>
+                            You will receive an amount of <mark>${interest}</mark>, <br/>
+                            in the year <mark>${year}</mark>`;
+
+    principalElement.focus();
 }
-        
+
+function updateRate() {
+    const rateval = document.getElementById('rate').value;
+    document.getElementById('rate_val').innerText = rateval + '%';
+}
